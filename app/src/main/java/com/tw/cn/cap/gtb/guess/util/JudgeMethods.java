8e78f356034840df53eab589ba877f533d713ada
Menghan Bai <majesty2@foxmail.com>
2022-10-31 18:07:02 +0800

    add: JudgeMethods model

---
+++ app/src/main/java/com/tw/cn/cap/gtb/guess/util/JudgeMethods.java
@@ -0,0 +1,11 @@
+package com.tw.cn.cap.gtb.guess.util;
+
+public class JudgeMethods {
+    static public boolean judgeIsSameAll(String random, String guess) {
+        return false;
+    }
+
+    static public boolean judgeIsExist(String random, String guess) {
+        return false;
+    }
+}
