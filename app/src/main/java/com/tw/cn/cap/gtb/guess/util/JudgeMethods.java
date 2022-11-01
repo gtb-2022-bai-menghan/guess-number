f7101772150ed8a4d5b4c3f6c1d90f2b0bac103f
Menghan Bai <majesty2@foxmail.com>
2022-11-01 11:13:37 +0800

    modify: The JudgeMethods param

--- app/src/main/java/com/tw/cn/cap/gtb/guess/util/JudgeMethods.java
+++ app/src/main/java/com/tw/cn/cap/gtb/guess/util/JudgeMethods.java
@@ -1,11 +1,20 @@
 package com.tw.cn.cap.gtb.guess.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class JudgeMethods {
-    static public boolean judgeIsSameAll(String random, String guess) {
+    static public boolean judgeIsSameAll(List<Integer> random, String guess, Integer station) {
+        for (int i = 0; i < random.size(); i++) {
+            if (random.get(i) == Integer.parseInt(guess)) {
+                return station == i;
+            }
+        }
         return false;
     }
 
-    static public boolean judgeIsExist(String random, String guess) {
-        return false;
+    static public boolean judgeIsExist(List<Integer> random, String guess) {
+        int num = Integer.parseInt(guess);
+        return random.contains(num);
     }
 }
